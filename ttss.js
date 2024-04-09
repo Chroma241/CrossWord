21a77e26c03fbda74476efef56bff9e04bb258f0
Fernand Mesange <82608094+Chroma241@users.noreply.github.com>
2024-04-09 22:57:38 +0100

    Add files via upload

---
+++ ttss.js
@@ -0,0 +1,46 @@
+var ttss = [
+	{
+		"word":"bangando",
+		"clue":"Il braque les gens"
+	},
+
+	{
+		"word":"musungu",
+		"clue":"C'est du vin de canne"
+	},
+
+	{
+		"word":"mbandja",
+		"clue":"Temple traditionnel, maison commune des hommes"
+	},
+	
+	{
+		"word":"bavette",
+		"clue":"Mesure barriere contre le coronavirus , en forme de tissu leger"
+	},
+	{
+		"word":"gorger",
+		"clue":"Parler francais avec un accent dit parisien"
+	},
+	{
+		"word":"ngata",
+		"clue":"Les jeunes ne disent pas prison"
+	},
+	{
+		"word":"boa",
+		"clue":"Il est mmediocre, assimile a un nom de serpent"
+	},
+	{
+		"word":"blaser",
+		"clue":"Frimer pour attirer l'attention"
+	},
+	{
+		"word":"soireau",
+		"clue":"Nom pour les eleves qui ont cours en soiree"
+	},
+	{
+		"word":"bondi",
+		"clue":"il t'a arnaqué"
+	},
+	
+]
